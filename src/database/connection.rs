88539f1675f2328f2390722f1c8d9bc5bc3ec0af
Elvin Sanchez <elvinmoises.sanchez@gmail.com>
2024-12-05 14:48:29 -0600

    fix(database): :fire: change type database connection

--- src/database/connection.rs
+++ src/database/connection.rs
@@ -1,42 +1,30 @@
-
 use std::sync::Arc;
-use surrealdb::engine::remote::ws::Ws;
+use surrealdb::engine::any;
 use surrealdb::opt::auth::Root;
 use surrealdb::Surreal;
-use surrealdb::engine::remote::ws::Client;
 
 use crate::handle_error::error::Error;
 
 pub struct DatabaseConnection {
-  db: Arc<Surreal<Client>>,
+  db: Arc<Surreal<any::Any>>,
 }
 
 impl DatabaseConnection {
   pub async fn new(url: &str, username: &str, password: &str, database_name: &str) -> Result<Self, Error> {
-    let db: Surreal<Client> = Surreal::init();
+    
+    let db = any::connect(url).await?;
 
-    let _  = db.connect::<Ws>(url).await?;
-
-    db.use_ns("").use_db(database_name).await?;
+    db.use_ns("dev").use_db(database_name).await?;
 
     db.signin(Root {
       username,
       password,
     }).await?;
 
-    match db.health().await {
-      Ok(_) => {
-          println!("SurrealDB is healthy!");
-      },
-      Err(e) => {
-          println!("SurrealDB is not healthy: {}", e);
-      }
-    }
-
     Ok(Self { db: Arc::new(db) })
   }
 
-  pub fn get_client(&self) -> Arc<Surreal<Client>> {
+  pub fn get_client(&self) -> Arc<Surreal<any::Any>> {
     self.db.clone()
   }
 }
