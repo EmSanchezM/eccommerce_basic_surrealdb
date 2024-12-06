5497bbcd9932af4e97ec5c7c5623de67eb39e6da
Elvin Sanchez <elvinmoises.sanchez@gmail.com>
2024-12-06 11:57:32 -0600

    fix(server-web): :bug: remove innecesary variables

--- src/configuration/server_web.rs
+++ src/configuration/server_web.rs
@@ -6,13 +6,13 @@ use crate::database::connection::DatabaseConnection;
 
 pub async fn run(config: Config) -> std::io::Result<()> {
   let config_init = config.clone();
-
-  let database_url = &config_init.database.url;
-  let database_name = &config_init.database.database_name;
-  let database_namespace = &config_init.database.database_namespace;
   let address = (config_init.server.host.as_str(), config_init.server.port);
 
-  let db = DatabaseConnection::new(database_url, database_name, database_namespace)
+  let db = DatabaseConnection::new(
+    &config_init.database.url.as_str(),
+    &config_init.database.database_name.as_str(),
+    &config_init.database.database_namespace.as_str()
+  )
       .await
       .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
     
